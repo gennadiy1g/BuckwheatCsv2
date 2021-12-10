4903869b24d5d63704f049ce0fdb20c972855fb7
Gennadiy <gennadiy1g@gmx.com>
2021-12-10 18:58:55 -0500

    VIP on App::OnInit().

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #endif
 
 #include <wx/docview.h>
+#include <wx/docmdi.h>
 
 #include "main.hpp"
 #include "csv_document.hpp"
@@ -24,6 +25,28 @@ bool App::OnInit()
     // Create a template relating text documents to their views
     new wxDocTemplate(docManager, "Text", "*.txt;*.text", "", "txt;text", "Text Doc", "Text View",
                       wxCLASSINFO(CsvDocument), wxCLASSINFO(CsvView));
+
+    auto frame = new wxDocMDIParentFrame(docManager, NULL, wxID_ANY,
+                                         GetAppDisplayName(),
+                                         wxDefaultPosition,
+                                         wxSize(500, 400));
+
+    auto *menuFile = new wxMenu;
+    menuFile->Append(wxID_OPEN);
+    menuFile->Append(wxID_CLOSE);
+    menuFile->Append(wxID_EXIT);
+    docManager->FileHistoryUseMenu(menuFile);
+
+    auto *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+
+    auto *menubar = new wxMenuBar;
+    menubar->Append(menuFile, wxGetStockLabel(wxID_FILE));
+    menubar->Append(menuHelp, wxGetStockLabel(wxID_HELP));
+
+    frame->SetMenuBar(menubar);
+    frame->Centre();
+    frame->Show();
 
     // Enter the application's main loop
     return true;
