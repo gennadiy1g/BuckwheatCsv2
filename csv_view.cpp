b8ad2501d6020279a56cb1abc4702f65d1d0e2dc
Gennadiy <gennadiy1g@gmx.com>
2022-02-02 15:25:30 -0500

    Replace __attribute__((unused)) with [[maybe_unused]].
    https://en.cppreference.com/w/cpp/language/attributes

--- csv_view.cpp
+++ csv_view.cpp
@@ -43,6 +43,6 @@ bool CsvView::OnClose(bool deleteWindow = true) {
 };
 
 void CsvView::OnThreadEvent(const wxThreadEvent &event) {
-  __attribute__((unused)) const std::size_t numLines = event.GetPayload<std::size_t>();
-  __attribute__((unused)) const int percent = event.GetInt();
+  [[maybe_unused]] const std::size_t numLines = event.GetPayload<std::size_t>();
+  [[maybe_unused]] const int percent = event.GetInt();
 };
