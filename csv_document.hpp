ed822a2905b82e4f631e7918c4e92c05d9e9fc7f
Gennadiy <gennadiy1g@gmx.com>
2022-02-02 14:35:13 -0500

    cosmetic

--- csv_document.hpp
+++ csv_document.hpp
@@ -30,7 +30,7 @@ public:
   virtual int GetNumberRows() override;
   virtual int GetNumberCols() override;
   virtual wxString GetValue(int row, int col) override;
-  virtual void SetValue(int row, int col, const wxString &value) override;
+  virtual void SetValue(int row, int col, const wxString &value) override{};
   virtual wxString GetColLabelValue(int col) override;
 
 private:
