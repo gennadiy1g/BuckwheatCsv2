46bb2b86c2fddf37f76d86bd3d1505b46a1478f8
Gennadiy <gennadiy1g@gmx.com>
2022-03-31 14:31:14 -0400

    WIP on CsvDocument::escapeSeparatorQuote().

--- csv_document.cpp
+++ csv_document.cpp
@@ -92,9 +92,7 @@ void CsvDocument::OnProgress(std::size_t numLines, int percent) {
 };
 
 void CsvDocument::escapeSeparatorQuote(wchar_t escape, wchar_t separator, wchar_t quote) {
-  if (separator != mSeparator || quote != mQuote || escape != mEscape) {
-    mpTokenizedFileLines->setTokenFuncParams(escape, separator, quote);
-  }
+  mpTokenizedFileLines->setTokenFuncParams(escape, separator, quote);
   mEscape = escape;
   mSeparator = separator;
   mQuote = quote;
