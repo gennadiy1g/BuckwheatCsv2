d684b1818dfabf08c468a59663986ead2e5b8107
Gennadiy <gennadiy1g@gmx.com>
2022-05-27 10:43:45 -0700

    Always refresh the grid in wxEVT_THREAD event hander.
    This behaviour is observed on Windows only.
    The grid remains empty until it is scrolled, or until grid's tab is
    deactivated and then activated back.

--- csv_view.cpp
+++ csv_view.cpp
@@ -225,13 +225,7 @@ void CsvView::OnThreadEvent(const wxThreadEvent &event) {
   mpGrid->SetTable(mpCsvGridTable);
 
   if (mIsActive) {
-    /* If the file is so small that its cells fit entirely inside the grid (in other words, the last cell is
-       visible), the grid appears empty, and ForceRefresh() is necessary to display the cells. */
-    if ((numLines > 1) && (mpGrid->IsVisible(numLines - 2, mpCsvGridTable->GetNumberCols() - 1, false))) {
-      BOOST_LOG_SEV(gLogger, trivial::trace) << "mpGrid->ForceRefresh()";
-      mpGrid->ForceRefresh();
-    }
-
+    mpGrid->ForceRefresh();
     showStatus();
   }
 };
