edac52260e8c1062362d8f9f2f0c1910d170124c
Gennadiy <gennadiy1g@gmx.com>
2022-06-11 18:17:36 -0400

    Add folder CsvTable.git to git daemon command.

--- git_daemon.sh
+++ git_daemon.sh
@@ -1,3 +1,3 @@
 # The bare repository is located at: /home/gennadiy/git/BuckwheatCsv.git
 
-git daemon --reuseaddr --verbose --base-path=/home/gennadiy/git --export-all --enable=receive-pack -- /home/gennadiy/git/BuckwheatCsv.git
+git daemon --reuseaddr --verbose --base-path=/home/gennadiy/git --export-all --enable=receive-pack -- /home/gennadiy/git/BuckwheatCsv.git /home/gennadiy/git/CsvTable.git
